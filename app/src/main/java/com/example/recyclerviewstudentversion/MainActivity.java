41f0b60961995ab0c2d2346a25d400638f4f89a8
mgrddsj <Jesse_Xu@live.com>
2019-11-28 08:59:47 +0800

    Finished ItemTouch Helper.
    Can now swipe and drag

--- app/src/main/java/com/example/recyclerviewstudentversion/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewstudentversion/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.recyclerviewstudentversion;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
 
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
@@ -19,28 +27,67 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getPlayers();
+
         recyclerView = findViewById(R.id.recyclerView);
-        mAdapter = new MyRecyclerAdapter(getPlayers());
+        mAdapter = new MyRecyclerAdapter((List<Player>)list);
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setAdapter(mAdapter);
         recyclerView.setLayoutManager(layoutManager);
+
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(ItemTouchHelper.UP | ItemTouchHelper.DOWN, ItemTouchHelper.LEFT)
+        {
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target)
+            {
+                final int fromPos = viewHolder.getAdapterPosition();
+                final int toPos = target.getAdapterPosition();
+                list.add(toPos, list.remove(fromPos));
+                mAdapter.notifyItemMoved(fromPos, toPos);
+                return true;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction)
+            {
+                final int position = viewHolder.getAdapterPosition();
+                final Player deleted = list.get(position);
+                switch (direction)
+                {
+                    case ItemTouchHelper.LEFT:
+                        list.remove(position);
+                        mAdapter.notifyItemRemoved(position);
+                        Snackbar.make(recyclerView, "Swiped left, item deleted. ", Snackbar.LENGTH_SHORT).
+                                setAction("Undo", new View.OnClickListener()
+                                {
+                                    @Override
+                                    public void onClick(View view)
+                                    {
+                                        list.add(position, deleted);
+                                        mAdapter.notifyItemInserted(position);
+                                    }
+                                }).show();
+                }
+            }
+        });
+        itemTouchHelper.attachToRecyclerView(recyclerView);
 
     }
 
     private List getPlayers()
     {
-      list = Arrays.asList(new Player[]{
-                new Player("Lionel Messi",32,44224400.00,"football",R.drawable.mesi),
-                new Player("David Robert Joseph Beckham",44,1291800.00,"football",R.drawable.beckham),
-                new Player("Hanyu Yuzulu",24,500000,"figure skating",R.drawable.hanyu),
-                new Player("Zhang Jike",31,500000,"Ping-pong",R.drawable.zhang),
-                new Player("Doris", 16, 100000, "Basketball", R.drawable.member_placeholder_female),
-                new Player("LeBron James", 34, 40000000, "Basketball", R.drawable.team_man_placeholder),
-                new Player("Kevin Durant", 31, 30000000, "Basketball", R.drawable.team_man_placeholder),
-                new Player("Kyrie Irving", 27, 20100000, "Basketball", R.drawable.team_man_placeholder),
-                new Player("James Harden", 30, 28300000, "Basketball", R.drawable.team_man_placeholder),
-                new Player("Stephen Curry", 31, 37460000, "Basketball", R.drawable.team_man_placeholder)
-        });
+      list = new ArrayList<Player>(Arrays.asList(new Player[]{
+              new Player("Lionel Messi", 32, 44224400.00, "football", R.drawable.mesi),
+              new Player("David Robert Joseph Beckham", 44, 1291800.00, "football", R.drawable.beckham),
+              new Player("Hanyu Yuzulu", 24, 500000, "figure skating", R.drawable.hanyu),
+              new Player("Zhang Jike", 31, 500000, "Ping-pong", R.drawable.zhang),
+              new Player("Doris", 16, 100000, "Basketball", R.drawable.member_placeholder_female),
+              new Player("LeBron James", 34, 40000000, "Basketball", R.drawable.team_man_placeholder),
+              new Player("Kevin Durant", 31, 30000000, "Basketball", R.drawable.team_man_placeholder),
+              new Player("Kyrie Irving", 27, 20100000, "Basketball", R.drawable.team_man_placeholder),
+              new Player("James Harden", 30, 28300000, "Basketball", R.drawable.team_man_placeholder),
+              new Player("Stephen Curry", 31, 37460000, "Basketball", R.drawable.team_man_placeholder)
+      }));
         return list;
     }
 }
